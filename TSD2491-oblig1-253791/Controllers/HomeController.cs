7c51e43af6498e10bfb2af5713d9fbbe797aba6a
253791 <156430068+253791@users.noreply.github.com>
2024-04-16 23:08:50 +0200

    Skjermbildet i webApp med ikoner (og tilfeldig plassering)

--- TSD2491-oblig1-253791/Controllers/HomeController.cs
+++ TSD2491-oblig1-253791/Controllers/HomeController.cs
@@ -27,8 +27,34 @@ namespace TSD2491_oblig1_253791.Controllers
                 "🐮", "🐮",
             };
 
+            List<string> shuffledAnimals = new List<string>();
+
             ViewData["AnimalEmoji"] = animalEmoji;
+            SetUpGame();
             return View();
+        }
+
+        private void SetUpGame()
+        {
+            var animalEmoji = ViewData["AnimalEmoji"] as List<string>;
+            if (animalEmoji != null)
+            {
+                ShuffleList(animalEmoji);           
+            }
+        }
+
+        private static void ShuffleList<T>(List<T> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            { // Fisher-Yates Shuffle Algorithm
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
 
         public IActionResult Privacy()
